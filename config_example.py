bdfb4a61009223ddd0cefafe8178aa88edc4fde7
SaltySOMAdmin <157048676+SaltySOMAdmin@users.noreply.github.com>
2025-05-07 09:23:56 -0400

    Create config_example.py

---
+++ config_example.py
@@ -0,0 +1,15 @@
+# Rename this file to config.py. Enter your credentials and save to the same folder as your script. If you're using a public repo then ensure this file is in your .gitignore list! 
+
+# SourceAccount
+source_client_id=""
+source_client_secret=""
+source_password=""
+source_username=""
+source_user_agent="<Ubuntu>.python:Archive.bot:v2.0.0 (by @saltysomadmin)"
+
+# DestinationAccount
+destination_client_id=""
+destination_client_secret=""
+destination_password=""
+destination_username=""
+destination_user_agent="<Ubuntu>.python:Archive.bot:v2.0.0 (by @saltysomadmin)"
